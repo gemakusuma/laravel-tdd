ff968aae308d892a6349e2979bb582fbe3f65f67
Gema Akbar <dityaksm21@gmail.com>
2019-12-04 20:42:02 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/blog', 'BlogController@index');
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::post('/blog', 'BlogController@store');
+    Route::get('/blog/create', 'BlogController@create');
+    Route::get('/blog/{slug}/edit', 'BlogController@edit');
+    Route::put('/blog/{id}', 'BlogController@update');
+});
+
+Route::get('/blog/{slug}', 'BlogController@show');
+
