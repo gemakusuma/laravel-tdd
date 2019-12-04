2cd5c08c647e39c50ea65486d5ed326600b0e32c
Gema Akbar <dityaksm21@gmail.com>
2019-12-04 20:38:31 +0700

    add migrations

---
+++ database/migrations/2019_11_25_025542_create_blog_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('subject');
+            $table->integer('user_id')->unsigned();
+            $table->integer('blog_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('blog_id')->references('id')->on('blogs')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_comments');
+    }
+}
